66a67286dd4e0e11c4d77b11c03ab16c08528501
Kevien-2020 <kevientie2005@yahoo.com.tw>
2020-01-19 03:27:08 +0800

    add file: countries.py

---
+++ Proj_2_Data_Visualization/countries.py
@@ -0,0 +1,15 @@
+"""Countries.py"""
+
+'''
+from pygal_maps_world.i18n import COUNTRIES
+
+for country_code in sorted(COUNTRIES.keys()):
+    print(country_code, COUNTRIES[country_code])
+'''
+
+
+from pygal.i18n import COUNTRIES
+
+for country_code in sorted(COUNTRIES.keys()):
+    print(country_code, COUNTRIES[country_code])
+
