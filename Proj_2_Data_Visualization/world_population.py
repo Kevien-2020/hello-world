0cddd36760082d14648060e8c66c0dc79f38ac05
Kevien-2020 <kevientie2005@yahoo.com.tw>
2020-01-19 03:08:35 +0800

    add file: world_population.py

---
+++ Proj_2_Data_Visualization/world_population.py
@@ -0,0 +1,24 @@
+"""World Population"""
+
+import json
+import os
+
+__location__ = os.path.realpath(os.path.join(
+    os.getcwd(), os.path.dirname(__file__)))
+
+# Load the data into a list.
+filename = 'population_data.json'
+openfile = open(os.path.join(
+    __location__, 'population_data.json'), encoding="utf8")
+
+# with open(os.path.join(sys.path[0], filename), "r") as f:
+
+with openfile as f:
+    pop_data = json.load(f)
+
+# Print the 2010 population for each country.
+for pop_dict in pop_data:
+    if pop_dict['Year'] == '2010':
+        country_name = pop_dict['Country Name']
+        population = int(float(pop_dict['Value']))
+        print(country_name + ": " + str(population))
