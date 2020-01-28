7754dd962fa4d95141bfb9fbd7d25bde9de0c9ab
Kevien-2020 <kevientie2005@yahoo.com.tw>
2020-01-29 02:21:55 +0800

    add pages (topic and entry)

--- Proj_3_Web_Application/learning_logs/views.py
+++ Proj_3_Web_Application/learning_logs/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+'''
+from django.shortcuts import render
+from django.http import HttpResponseRedirect, Http404
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+'''
+
 from .models import Topic
+from .forms import TopicForm, EntryForm
+
+'''
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
+'''
+
 
 def index(request):
     """The home page for Learning Log."""
     return render(request, 'learning_logs/index.html')
+
 
 def topics(request):
     """Show all topics."""
@@ -11,11 +29,13 @@ def topics(request):
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
+
 def test_page_1(request):
     """The home page for Learning Log."""
     test = 1+1
     context = {'test0001': test}
     return render(request, 'learning_logs/test_page_1.html', context)
+
 
 def topic(request, topic_id):
     """Show a single topic and all its entries."""
@@ -23,3 +43,39 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+
+def new_topic(request):
+    """Add a new topic."""
+    if request.method != 'POST':
+        # No data submitted; create a blank form.
+        form = TopicForm()
+    else:
+        # Post data submitted; process data.
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+    """Add a new entry for a particular topic."""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        # No data submitted; create a blank form.
+        form = EntryForm()
+    else:
+        # Post data submitted; process data.
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic_id]))
+
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
